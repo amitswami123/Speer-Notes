a932bdc2a149816422e441034496e8d1e4363b29
Amit Swami <amit.swami@pw.live>
2024-01-04 02:15:07 +0530

    Added throttling and rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/user.module';
 import { NotesModule } from './modules/notes/notes.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
 
 @Module({
-  imports: [UsersModule,NotesModule, MongooseModule.forRoot('mongodb://localhost:27017/speer'), AuthModule],
+  imports: [UsersModule,NotesModule, MongooseModule.forRoot('mongodb://localhost:27017/speer'), AuthModule,
+  ThrottlerModule.forRoot([{
+    ttl: 60000,
+    limit: 10,
+  }]),
+],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,{
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }
+  ],
 })
 export class AppModule {}
