1e37fe469118693de044fc66cafa94161730e4f6
Amit Swami <amit.swami@pw.live>
2024-01-04 01:31:56 +0530

    updated packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { UsersModule } from './modules/users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/user.module';
+import { NotesModule } from './modules/notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule,NotesModule, MongooseModule.forRoot('mongodb://localhost:27017/speer'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
